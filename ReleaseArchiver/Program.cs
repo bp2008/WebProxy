115e2c677240f18c3cc382dcc15b404f0c7e2e87
bp2008 <bp2008@users.noreply.github.com>
2024-11-18 10:20:02 -0700

    Version 1.29.0.1: * Release archiver now verifies the admin console webapp files exist.

--- ReleaseArchiver/Program.cs
+++ ReleaseArchiver/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.IO.Compression;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace ReleaseArchiver
 {
@@ -18,23 +19,49 @@ namespace ReleaseArchiver
 
 		private static void CreateRelease(string folder, string name, string fileForVersioning)
 		{
-			string releasePath = "../../../../WebProxy/bin/Release/" + folder;
-			string dllPath = Path.Combine(releasePath, fileForVersioning);
-			string version = AssemblyName.GetAssemblyName(dllPath).Version.ToString();
-			string zipName = name + " " + version + ".zip";
-			string releasesDir = "../../../../Releases";
-			string zipPath = Path.Combine(releasesDir, zipName);
-
-			if (File.Exists(zipPath))
-				Console.WriteLine(zipName + " ALREADY EXISTS");
-			else
+			string version = "";
+			try
 			{
-				Console.WriteLine("Creating " + zipName);
-				if (!Directory.Exists(releasesDir))
-					Directory.CreateDirectory(releasesDir);
+				string releasePath = "../../../../WebProxy/bin/Release/" + folder;
+				string dllPath = Path.Combine(releasePath, fileForVersioning);
+				version = AssemblyName.GetAssemblyName(dllPath).Version.ToString();
+				string zipName = name + " " + version + ".zip";
+				string releasesDir = "../../../../Releases";
+				string zipPath = Path.Combine(releasesDir, zipName);
 
-				ZipFile.CreateFromDirectory(releasePath, zipPath);
+				if (File.Exists(zipPath))
+					Console.WriteLine(zipName + " ALREADY EXISTS");
+				else
+				{
+					DirectoryInfo diReleaseSourceDir = new DirectoryInfo(releasePath);
+					FileInfo[] diReleaseSourceFiles = diReleaseSourceDir.GetFiles("*", SearchOption.AllDirectories);
+					string[] releaseSourceFilePaths = diReleaseSourceFiles.Select(fi => fi.FullName.Replace('\\', '/')).ToArray();
+					if (!releaseSourceFilePaths.Any(p => p.EndsWith("/www/index.html")))
+						throw new Exception(folder + "/www/index.html not found.");
+					if (!releaseSourceFilePaths.Any(p => Regex.IsMatch(p, "/www/assets/index-(.+)\\.js$")))
+						throw new Exception(folder + "/www/assets/index-*.js not found.");
+					if (!releaseSourceFilePaths.Any(p => Regex.IsMatch(p, "/www/assets/index-(.+)\\.css$")))
+						throw new Exception(folder + "/www/assets/index-*.css not found.");
+
+					Console.WriteLine("Creating " + zipName);
+					if (!Directory.Exists(releasesDir))
+						Directory.CreateDirectory(releasesDir);
+
+					ZipFile.CreateFromDirectory(releasePath, zipPath);
+				}
 			}
+			catch (Exception ex)
+			{
+				WriteError(ex.Message);
+				WriteError("Release " + version + " not created properly.");
+			}
+		}
+
+		private static void WriteError(string message)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Error.WriteLine(message);
+			Console.ResetColor();
 		}
 	}
 }
